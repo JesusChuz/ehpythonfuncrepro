df5749f215148e70224774f5a3e4e728a57972d1
unknown <j.mendezquesada18@gmail.com>
2023-10-20 11:28:53 -0600

    create func 1

--- eh2/__init__.py
+++ eh2/__init__.py
@@ -1,10 +1,19 @@
 from typing import List
+import datetime
 import logging
 
 import azure.functions as func
 
 
 def main(events2: List[func.EventHubEvent]):
-     for event in events2:
-         logging.info('Python EventHub trigger processed an event: %s',
+        for event in events2:
+             logging.info('Python EventHub trigger processed an event: %s',
                          event.get_body().decode('utf-8'))
+
+#def main(events2: List[func.EventHubEvent], outputEvent2: func.Out[str]):
+
+#     for event in events2:
+#         event_data = event.get_body().decode('utf-8')
+#         logging.info('Python EventHub trigger processed an event: %s',
+#                        event.get_body().decode('utf-8'))
+#         outputEvent2.set(event_data)
