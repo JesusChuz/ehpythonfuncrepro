636fdc2150f6cb44a21173ec78a9e0bae8f8102a
unknown <j.mendezquesada18@gmail.com>
2023-10-25 09:52:13 -0600

    add funcapp2partitions

--- eh1/__init__.py
+++ eh1/__init__.py
@@ -4,17 +4,17 @@ import logging
 
 import azure.functions as func
 
-#def main(events: List[func.EventHubEvent]):
-#        for event in events:
-#             logging.info('Python EventHub trigger processed an event: %s',
-#                         event.get_body().decode('utf-8'))
+def main(events: List[func.EventHubEvent]):
+        for event in events:
+             logging.info('Python EventHub trigger processed an event: %s',
+                         event.get_body().decode('utf-8'))
 
-def main(events: List[func.EventHubEvent], outputEvent: func.Out[str]):
-     for event in events:
-         event_data = event.get_body().decode('utf-8')
-         logging.info('Python EventHub trigger processed an event: %s',
-                        event.get_body().decode('utf-8'))
-         outputEvent.set(event_data)
+#def main(events: List[func.EventHubEvent], outputEvent: func.Out[str]):
+#     for event in events:
+#         event_data = event.get_body().decode('utf-8')
+#         logging.info('Python EventHub trigger processed an event: %s',
+#                        event.get_body().decode('utf-8'))
+#         outputEvent.set(event_data)
      
 
 
